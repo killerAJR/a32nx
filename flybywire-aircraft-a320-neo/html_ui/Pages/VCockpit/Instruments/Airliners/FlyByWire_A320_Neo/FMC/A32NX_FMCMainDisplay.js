3dbe201cd3c3bca37cdc35a22d6bc4d93a24b85d
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-09-16 22:19:32 +0200

    Hide flight plan predictions in recomputation

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -2801,6 +2801,7 @@ class FMCMainDisplay extends BaseAirliners {
                         this.clearAutotunedIls();
                         this.tempFpPendingAutoTune = false;
                     }
+                    this.guidanceController.vnavDriver.invalidateFlightPlanProfile();
                     callback();
                 });
             }).catch(console.error);
